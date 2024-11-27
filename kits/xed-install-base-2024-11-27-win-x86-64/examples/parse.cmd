aae544d2ea813e69bf06a27e8610f3bc2d717f33
Admin <2762713521@qq.com>
2024-11-27 16:19:17 +0800

    add xed

--- kits/xed-install-base-2024-11-27-win-x86-64/examples/parse.cmd
+++ kits/xed-install-base-2024-11-27-win-x86-64/examples/parse.cmd
@@ -21,4 +21,28 @@ asmparse -q lock mov dword ptr [ebx], esi
 asmparse -q lock mov edi, dword ptr [ecx]
 asmparse -q call far 0x1234:0x10dedead
 asmparse -q call 0x1234:0x10dedead
-pause
+
+asmparse -q mov dword ptr ds:[edi+5E8],eax
+asmparse  mov dword ptr ds:[edi+5E8],eax
+
+asmparse -q call 7FFC0A2B66B0
+asmparse -q jmp 7FFC0A36B4C3
+asmparse -q mov rax,qword ptr ss:[rsp+40]
+asmparse -q or dword ptr ds:[rax+68],r14d
+asmparse -q mov rax,qword ptr ss:[rsp+40]
+asmparse -q mov rcx,qword ptr ds:[rax+30]
+asmparse -q mov qword ptr ds:[7FFC0A4143A8],rcx
+asmparse -q call 7FFC0A3093C8
+asmparse -q mov ebx,eax
+asmparse -q test eax,eax
+asmparse -q jns 7FFC0A36B491
+asmparse -q mov dword ptr ss:[rsp+28],eax
+asmparse -q lea r8,qword ptr ds:[7FFC0A3DD270]
+asmparse -q lea rax,qword ptr ds:[7FFC0A3DD148]
+asmparse -q xor r9d,r9d
+asmparse -q mov edx,DF2
+asmparse -q mov qword ptr ss:[rsp+20],rax
+asmparse -q lea rcx,qword ptr ds:[7FFC0A3C6C08]
+asmparse -q call 7FFC0A2B66B0
+asmparse -q jmp 7FFC0A36B4C3
+          pause
