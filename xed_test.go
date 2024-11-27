5e9ba683899171eb8a3cccf999368c54629d6957
Admin <2762713521@qq.com>
2024-11-27 22:35:16 +0800

    start binding

--- xed_test.go
+++ xed_test.go
@@ -17,6 +17,8 @@ import (
 func TestMergeHeader(t *testing.T) {
 	names := stream.NewOrderedMap("", 0)
 	debugIndex := 0
+
+	var allNames []string
 	filepath.Walk("kits/xed-install-base-2024-11-27-win-x86-64/include/xed", func(path string, info fs.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
@@ -42,90 +44,25 @@ func TestMergeHeader(t *testing.T) {
 		}
 		debugIndex++
 		mylog.Info(debugIndex, filepath.Base(path))
+		allNames = append(allNames, filepath.Base(path))
 		return err
 	})
 	for _, p := range names.List() {
 		mylog.Success(p.Key, p.Value)
 	}
+
+	last := names.Last()
+	lastIndex := last.Value
+
+	for _, name := range allNames {
+		lastIndex++
+		names.Set(name, lastIndex)
+	}
+	for _, p := range names.List() {
+		mylog.Success(p.Key, p.Value)
+	}
 }
 
-// xed-address-width-enum.h
-// xed-agen.h
-// xed-attribute-enum.h
-// xed-attributes.h
-// xed-build-defines.h
-// xed-category-enum.h
-// xed-chip-enum.h
-// xed-chip-features.h
-// xed-common-defs.h
-// xed-common-hdrs.h
-// xed-convert-table-init.h
-// xed-cpuid-group-enum.h
-// xed-cpuid-rec-enum.h
-// xed-cpuid-rec.h
-// xed-decode.h
-// xed-decoded-inst-api.h
-// xed-decoded-inst.h
-// xed-disas.h
-// xed-encode-check.h
-// xed-encode-direct.h
-// xed-encode.h
-// xed-encoder-gen-defs.h
-// xed-encoder-hl.h
-// xed-encoder-iforms.h
-// xed-error-enum.h
-// xed-exception-enum.h
-// xed-extension-enum.h
-// xed-flag-action-enum.h
-// xed-flag-enum.h
-// xed-flags.h
-// xed-format-options.h
-// xed-gen-table-defs.h
-// xed-get-time.h
-// xed-iclass-enum.h
-// xed-iform-enum.h
-// xed-iform-map.h
-// xed-iformfl-enum.h
-// xed-ild-enum.h
-// xed-ild.h
-// xed-immdis.h
-// xed-immed.h
-// xed-init-pointer-names.h
-// xed-init.h
-// xed-inst.h
-// xed-interface.h
-// xed-isa-set-enum.h
-// xed-isa-set.h
-// xed-machine-mode-enum.h
-// xed-mapu-enum.h
-// xed-nonterminal-enum.h
-// xed-operand-accessors.h
-// xed-operand-action-enum.h
-// xed-operand-action.h
-// xed-operand-convert-enum.h
-// xed-operand-ctype-enum.h
-// xed-operand-ctype-map.h
-// xed-operand-element-type-enum.h
-// xed-operand-element-xtype-enum.h
-// xed-operand-enum.h
-// xed-operand-storage.h
-// xed-operand-type-enum.h
-// xed-operand-values-interface.h
-// xed-operand-visibility-enum.h
-// xed-operand-width-enum.h
-// xed-patch.h
-// xed-portability.h
-// xed-print-info.h
-// xed-reg-class-enum.h
-// xed-reg-class.h
-// xed-reg-enum.h
-// xed-reg-role-enum.h
-// xed-rep-prefix.h
-// xed-state.h
-// xed-syntax-enum.h
-// xed-types.h
-// xed-util.h
-// xed-version.h
 func TestBindXed(t *testing.T) {
 	TestMergeHeader(t)
 	pkg := gengo.NewPackage("xed")
